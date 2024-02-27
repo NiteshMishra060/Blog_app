33e3e89881c63609c64546f486a4279993f6978e
NITESH MISHRA <niteshmishra060@gmail.com>
2024-02-27 20:02:42 +0530

    add schema for user comment

---
+++ backend/model/userComment.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose");
+const schema=mongoose.Schema;
+const userComment=new schema({
+    name:{
+        type:String,
+        required:true
+    },
+    comment:{
+        type:String,
+        required:true
+    }
+});
+export default mongoose.model("user Comments",userComment);
